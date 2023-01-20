a221f1915ed6d9f70fb2b1eb15db48394fe127ac
hw1 <lilia_dali@mail.ru>
2023-01-21 05:27:08 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+let a = 10;
+
+alert(a);
+
+a += 10;
+
+alert(a);
+
+let iphoneBirthday = 2007;
+
+alert(iphoneBirthday);
+
+let creator = "Брендан Эйх";
+
+alert(creator);
+
+let room = 10;
+
+let number = 2;
+
+let amount = room + number;
+
+let subtraction = room - number;
+
+let multipiy = room * number;
+
+let share = room / number;
+
+alert(`${amount} ${subtraction} ${multipiy} ${share}`);
+
+let texRoom = 2;
+let texNumber = 5;
+let result = texRoom ** texNumber;
+alert(result);
+
+let textA = 9;
+let b = 2;
+let remains = textA % b;
+alert(remains);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let years = prompt("Сколько вам лет?");
+alert(prompt);
+
+let user = {
+    name:"Gena",
+    age: 30, 
+    isAdmin:("0")
+}
+
+console.log(typeof isAdmin); 
+
+console.log(user["city of residence"])
+user["city of residence"] = true;
+
+console.log(user.age);
+
+user.age = 3;
+
+delete user["city of residence"];
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+
+console.log(info.user);
+
+alert(info.user);
+
+let nameText = prompt('Введите ваше имя');
+
+alert(`Привет, ${nameText}!`);
+
+
+
+
+
+
